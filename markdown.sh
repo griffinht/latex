dbf6530ad0388ba1f57bd6df2548f2c86258ac96
stzups <griffinht@gmail.com>
2022-02-16 22:22:39 -0500

    fix nested directory paths

--- markdown.sh
+++ markdown.sh
@@ -5,11 +5,11 @@ if [ -z "$FILE" ]; then
   FILE='*'
 fi
 
-inotifywait -e close_write -m -r --format %f . | 
-  while read -r file; do 
+inotifywait -e close_write -m -r --format %w%f . |
+  while read -r file; do
     echo "$file"
     if [[ "$file" == $FILE.md ]]; then
-      pandoc -i "$file" -o "$(echo $file | cut -f 1 -d '.').pdf";
+      pandoc -i "$file" -o "$(echo $file | rev | cut -f 2- -d '.' | rev).pdf";
     fi;
   done
 
